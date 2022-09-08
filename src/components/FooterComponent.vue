c63dc428d7b4e6917b53cd483765dc1fb8ba71ee
Douglas Lopes <pchialoopes@gmail.com>
2022-09-08 12:42:02 -0300

    Adicionando footer a aplicação

---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <div class="card text-center">
+            <div class="card-body">
+                <p class="card-text">Direitos reservado ao desenvolvedor.</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'FooterComponent'
+    }
+</script>
